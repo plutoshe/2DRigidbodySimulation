a9b9ed2ed21f579370780423168c86dd5e3eb1a2
plutoshe <plutoshe@gmail.com>
2019-06-27 17:04:59 +0800

    default setting for 2d rendering

--- src/js/main.js
+++ src/js/main.js
@@ -8,29 +8,53 @@ function init()
 {
 	var display = $('#display')[0];
     var scene = new THREE.Scene();
-	var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+ 	var camera = new THREE.PerspectiveCamera( 90, 1, 0.1, 500 );
 
 	var renderer = new THREE.WebGLRenderer();
 	var width = max(display.clientWidth, 100);
     var height = max(display.clientHeight, 100);
     renderer.setSize(width, height);
 	display.appendChild(renderer.domElement);
-	var geometry = new THREE.BoxGeometry( 1, 1, 1 );
 	var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+	// this.materials.setBodyData = new THREE.ShaderMaterial({
+ //        uniforms: {
+ //            res: { value: new THREE.Vector2() }
+ //        },
+ //        vertexShader: getShader( 'setBodyDataVert' ),
+ //        fragmentShader: getShader( 'setBodyDataFrag' ),
+ //        defines: this.getDefines()
+ //    });
+
+	var geometry = new THREE.Geometry();
+	var size = 100;
+	geometry.vertices.push(
+		new THREE.Vector3( -size,  size, 0 ),
+		new THREE.Vector3( -size, -size, 0 ),
+		new THREE.Vector3(  size, -size, 0 ),
+		new THREE.Vector3(  size, size, 0 ),
+	);
+
+	geometry.faces.push( new THREE.Face3( 0, 1, 2 ), new THREE.Face3( 0, 2, 3) );
+
+	geometry.computeBoundingSphere();
 	var cube = new THREE.Mesh( geometry, material );
 	scene.add( cube );
+	camera.position.x = 0;
+	camera.position.y = 0;
+	camera.position.z = 100;
+	cube.position.z = 0;
+	renderer.render( scene, camera );
+	 
+	// var animate = function () 
+	// {
+	// 	requestAnimationFrame( animate );
 
-	camera.position.z = 5;
-	var animate = function () 
-	{
-		requestAnimationFrame( animate );
+	// 	cube.rotation.x += 0.01;
+	// 	cube.rotation.y += 0.01;
 
-		cube.rotation.x += 0.01;
-		cube.rotation.y += 0.01;
+	// 	renderer.render( scene, camera );
+	// };
 
-		renderer.render( scene, camera );
-	};
-
-	animate();
+	// animate();
 }
 
---
+++ src/js/shader.js
@@ -0,0 +1,7 @@
+import generalVert from '../glsl/generalVert.glsl'
+import generalFrag from '../glsl/generalFrag.glsl'
+
+var shaders = {
+	generalVert,
+	generalFrag,
+}
